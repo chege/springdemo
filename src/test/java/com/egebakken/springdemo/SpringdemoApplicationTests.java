4e2e327e88376b244afc6ed31f12f8a0c1ce1399
Christopher Egebakken <Christopher.Egebakken@norwegian.com>
2019-09-11 21:12:35 +0200

    init

---
+++ src/test/java/com/egebakken/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egebakken.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
