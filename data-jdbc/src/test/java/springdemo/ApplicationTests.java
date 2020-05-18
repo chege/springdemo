ef77829420014c2324d3ebc0f40e4f786b3b4aec
Christopher Egebakken <Christopher.Egebakken@norwegian.com>
2020-05-19 00:04:10 +0200

    add application tests

--- data-jdbc/src/test/java/springdemo/ApplicationTests.java
+++ data-jdbc/src/test/java/springdemo/ApplicationTests.java
@@ -1,15 +1,25 @@
 package springdemo;
 
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class ApplicationTests {
@@ -20,15 +30,18 @@ public class ApplicationTests {
     @Autowired
     private WebTestClient client;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    public void findAllUsers() {
+    public void _findAllUsers() { // starts with underscore to force junit 4 to run it before methods mutating the DB. JUnit5 has an @Order annotation
         client.get()
               .uri("users")
               .exchange()
               .expectStatus()
               .isOk()
               .expectBody()
-              .json(String.format(
+              .json(format(
                       "{\n"
                               + "  \"_embedded\": {\n"
                               + "    \"userList\": [\n"
@@ -63,14 +76,14 @@ public class ApplicationTests {
     }
 
     @Test
-    public void getUser() {
+    public void _getUser() {
         client.get()
               .uri("users/1")
               .exchange()
               .expectStatus()
               .isOk()
               .expectBody()
-              .json(String.format(
+              .json(format(
                       "{\n"
                               + "  \"id\": 1,\n"
                               + "  \"username\": \"prince\",\n"
@@ -83,5 +96,82 @@ public class ApplicationTests {
                               + "}", port
               ));
 
+    }
+
+    @Test
+    public void getNonExistentUser() {
+        client.get()
+              .uri("users/999")
+              .exchange()
+              .expectStatus()
+              .isNotFound();
+    }
+
+
+    @Test
+    public void addUser() {
+        client.post()
+              .uri("users")
+              .contentType(MediaType.APPLICATION_JSON)
+              .bodyValue("{\n"
+                                 + "  \"username\": \"alex\",\n"
+                                 + "  \"email\":\"alex.lifeson@rush.com\"\n"
+                                 + "}")
+              .exchange()
+              .expectStatus()
+              .isCreated()
+              .expectHeader()
+              .valueMatches("Location", format("http://localhost:%s/users/3", port));
+
+        assertThat(jdbcTemplate.queryForMap(format("select * from user where id=%d", 3)))
+                .contains(entry("username", "alex"))
+                .contains(entry("email", "alex.lifeson@rush.com"));
+    }
+
+    @Test
+    public void addExistentUsername() {
+        // using the count from the db removes the need to update the test if more test data are added to the test in-mem DB (data.sql)
+        int previousCount = JdbcTestUtils.countRowsInTable(jdbcTemplate, "user");
+
+        client.post()
+              .uri("users")
+              .contentType(MediaType.APPLICATION_JSON)
+              .bodyValue("{\n"
+                                 + "  \"username\": \"elvis\",\n"
+                                 + "  \"email\":\"elvis.ellingsen@sol.no\"\n"
+                                 + "}")
+              .exchange()
+              .expectStatus()
+              .isEqualTo(HttpStatus.CONFLICT);
+
+        assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "user"))
+                .isEqualTo(previousCount);
+    }
+
+    @Test
+    public void deleteUser() {
+        client.delete()
+              .uri("users/1")
+              .exchange()
+              .expectStatus()
+              .isNoContent();
+
+        assertThat(jdbcTemplate.queryForList(format("select * from user where id=%d", 1)))
+                .isEmpty();
+    }
+
+    @Test
+    public void deleteNonExistentUser() {
+        // using the count from the db removes the need to update the test if more test data are added to the test in-mem DB (data.sql)
+        int previousCount = JdbcTestUtils.countRowsInTable(jdbcTemplate, "user");
+
+        client.delete()
+              .uri("users/999")
+              .exchange()
+              .expectStatus()
+              .isNotFound();
+
+        assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "user"))
+                .isEqualTo(previousCount);
     }
 }
